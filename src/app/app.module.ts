3fccead68cd0fae5fe88fd26a154c029e33f5d44
Reda <reda.boussabat@ucll.be>
2020-12-17 00:52:51 +0100

    bijna af

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  providers: [ UserService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/user.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+
+import {User} from './user';
+
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class UserService {
+  private userUrl = 'http://localhost:8080/Controller?command=SendUsers';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.userUrl);
+  }
+
+
+  changeUserInfo(thisUser: User): void {
+    let body = new HttpParams().append('firstName', thisUser.firstName).append('email', thisUser.email);
+
+    const header = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    this.http.post<any>(this.userUrl, body, {headers: header}).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err));
+  }
+}
